e4629fa7085e6017cfc5f1d7f202446f7de2d21c
Abdul-majid-ashrafi <dear_majid01@yahoo.com>
2018-05-13 10:30:18 +0500

    First Lab of javascript

---
+++ JAVASCCRIPT/5-13-18/app.js
@@ -0,0 +1,205 @@
+// /** blank **/
+// alert();
+
+// /** strings **/
+// alert("SSUET"); // double quatation with string
+// alert('SSUET'); // single quatation with string
+// alert("123"); // double quatation with number
+// alert("Hello World!"); // double quatation with string
+
+// alert(     "Hello World!"     );
+
+// alert("Hello             World!");
+
+// /** numbers **/
+// alert(123);
+// alert(1.23);
+
+
+
+
+
+
+
+/** variable declaration **/
+// var country,myName;
+// alert(country); // undefine
+
+
+
+
+
+
+
+
+/** variable initialization **/
+//   country = "Pakistan";
+//   alert(country);
+
+
+
+
+
+
+
+
+//   var studentName = 'ABC XYZ';
+
+//   var password = "pakistan";
+
+//   var rollNumber = "A123";
+//   var registrationNumber = "A123";
+//   var email = "abc@xyz.com";
+//   alert(email);
+//   email = "pakistan@gmail.com";
+//   alert(email);
+
+
+
+
+
+
+// var 2;            //illegal
+// var 2things;      //illegal
+// var .things;      //illegal
+// var %things;      //illegal
+// var -things;      //illegal
+// var @things;      //illegal
+// var _things;      //legal
+// var $things;      //legal
+// var things2;      //legal
+// var twoThings;    //legal
+// var TwoThings;    //legal
+// var Two2Things;   //legal
+
+
+
+
+
+
+
+
+
+
+
+//   Variables for Numbers, Math Expressions
+
+//   var num = 2;
+//   var anotherNum = 3.2;
+
+//   alert(num);
+//   alert(anotherNum);
+//   alert(num + 3);
+
+
+/** add two numbers **/
+//   var newNumber   = 4 + 3; // num and more
+//   alert(newNumber);
+
+
+
+
+
+
+//   var age = 19;
+//   var newAge = age + 3;
+//   alert(newAge , typeof age);
+//   alert(typeof age);
+
+
+
+
+
+//var 2; //Not allowed
+var two;
+
+
+
+
+/** Increment / Decrement Operators  **/
+
+// var count = 5;
+// var updatedCount = count + 1;
+
+// alert(updatedCount);
+// alert(count);
+
+
+
+
+
+
+var age = 12;
+
+/** alternate ways to increment a value  **/
+age = age + 1;
+age += 1;
+age++; //increment operator
+
+
+/** alternate ways to decrement a value  **/
+age = age - 1;
+age -= 1;
+age--; //decrement operator
+
+
+/** increment 3 numbers in a value  **/
+age = age + 3;
+age += 3;
+
+
+//   var count = 19;
+//   count--;
+//   alert(count);
+//   count--;
+//   alert(count);
+
+
+/** Post Increment & Pre Increment Operators  **/
+
+var num = 2;
+num++; //post increment operator
+++num; //pre increment operator
+
+
+//   /** post increment in an expression  **/
+//   var age     = 20;
+//   var newAge  = age++;
+//   alert(newAge);
+//   alert(age);
+
+//   /** pre increment in an expression  **/
+//   var age     = 20;
+//   var newAge  = ++age;
+//   alert(newAge);
+//   alert(age);
+
+
+
+
+
+
+
+
+
+
+
+
+
+var table = prompt()
+document.write("<h1>" + table + " X " + 1 + " = " + table * 1 + "</h1>")
+document.write("<h1>" + table + " X " + 2 + " = " + table * 2 + "</h1>")
+document.write("<h1>" + table + " X " + 3 + " = " + table * 3 + "</h1>")
+document.write("<h1>" + table + " X " + 4 + " = " + table * 4 + "</h1>")
+document.write("<h1>" + table + " X " + 5 + " = " + table * 5 + "</h1>")
+
+
+
+
+
+
+
+
+
+
+// TABLE 2 x 1 = 2;
